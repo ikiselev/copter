8b9c2ac794111bdf1c97915c82502b1ec77c6b2a
ikiselev <vanwin@yandex.ru>
2013-12-29 03:16:17 +0400

    Constants. Debug with PGM pointer. Debug for sensors. Logger type now is toggle 0-1 values

--- Copter/constants.h
+++ Copter/constants.h
@@ -1,19 +1,36 @@
+#ifndef __CONSTANTS_
+#define __CONSTANTS_
+
 /**
- * Константы для класса Logger
+ * Какой из типов включен
  */
-#define LOGGER_NONE 0
 #define LOGGER_SD_CARD 1
-#define LOGGER_SERIAL 2
+#define LOGGER_SERIAL 0
 
 
-#define DEBUG_ENABLE 0
+#define DEBUG_ENABLE 1
+#define DEBUG_SENSORS_ENABLE 0
 
+#ifdef MAIN_FILE
+char temp_buffer[40];
+#else
+extern char temp_buffer[40];
+#endif
+
+#define P(str) (strcpy_P(temp_buffer, PSTR(str)), temp_buffer)
 
 
 #if DEBUG_ENABLE
+    #include <avr/pgmspace.h>
+
     inline void debug(const char *s) { Serial.println(s); }
     inline void debug(const char *s, char *t) { Serial.print(s);Serial.println(t); }
+    inline void debug(const char *s, float t) { Serial.print(s);Serial.println(t); }
 #else
-inline void debug(const char *s) {  }
-inline void debug(const char *s, const char *t) { }
+    inline void debug(const char *s) {  }
+    inline void debug(const char *s, const char *t) { }
+    inline void debug(const char *s, float t) { }
+#endif
+
+
 #endif
